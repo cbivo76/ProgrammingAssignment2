400dd32defc8dea2b8b29b8bfbc8ce340e889b79
unknown <cbivo76@gmail.com>
2015-08-23 10:17:06 -0400

    For coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+# returns a list of functions
+# it caches an inverse of a matrix given as input parameter
+#	makeCacheMatrix
+# 	a <- makeCacheMatrix( matrix(c(1,2,12,13), nrow = 2, ncol = 2) );
+# 	summary(a)
+#	a$getMatrix();
+#	cacheSolve(a)
+#	cacheSolve(a)
+ 
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y){
-    x<<-y
-    m<<-NULL
-  }
-  get<-function() x
-  setmatrix<-function(solve) m<<- solve
-  getmatrix<-function() m
-  list(set=set, get=get,
-       setmatrix=setmatrix,
-       getmatrix=getmatrix)
+       
+	# Cached value, NULL if nothing
+	m <- NULL
+				        
+	setMatrix <- function(newValue) {
+		x <<- newValue
+	        m <<- NULL
+	}
+
+	getMatrix <- function() {
+	        x
+	}
+
+       	cacheInverse <- function(solve) {
+	        m <<- solve
+	}
+										        getInverse <- function() {
+	        m
+	}
+													        
+	list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
-cacheSolve <- function(x=matrix(), ...) {
-  m<-x$getmatrix()
-  if(!is.null(m)){
-    message("getting cached data")
-    return(m)
-  }
-  matrix<-x$get()
-  m<-solve(matrix, ...)
-  x$setmatrix(m)
-  m
-}
+cacheSolve <- function(y, ...) {
+        inverse <- y$getInverse()
+										        if(!is.null(inverse)) {
+		message("getting cached data")
+	        return(inverse)
+	}
+
+	data <- y$getMatrix()
+	inverse <- solve(data)
+        y$cacheInverse(inverse)
+	inverse
+}	
